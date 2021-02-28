e4222e04ca069ee2fcf27a0ac0bd3054c4322f2a
Hari Om <58305594+Madhav008@users.noreply.github.com>
2021-03-01 05:15:45 +0530

    My First React App

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+const Header = (props) => {
+    const onClick = () => {
+    console.log("click");
+  };
+  return (
+    <header className="header">
+      <h1>{props.title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+      
+    </header>
+  );
+};
+
+Header.defaultProps = {
+  title: "Task Traker",
+};
+Header.propTypes = {
+  title: PropTypes.string,
+};
+
+export default Header;
